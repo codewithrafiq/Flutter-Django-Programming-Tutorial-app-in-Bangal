deb8c4afd40b555d3d7580cdf22cc0ad55adf682
codewithrafiq <codewithrafiq@gmail.com>
2021-02-03 14:20:55 +0600

    part-5

--- djangof/blog/serializers.py
+++ djangof/blog/serializers.py
@@ -1,5 +1,15 @@
 from rest_framework import serializers
 from .models import *
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class UserSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password', 'email',)
+        extra_kwargs = {'password': {"write_only": True, 'required': True}}
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -13,12 +23,24 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = "__all__"
+        # depth = 1
+
+    def to_representation(self, instance):
+        response = super().to_representation(instance)
+        response['user'] = UserSerializers(instance.user).data
+        return response
 
 
 class ReplySerializer(serializers.ModelSerializer):
     class Meta:
         model = Reply
         fields = "__all__"
+        # depth = 1
+
+    def to_representation(self, instance):
+        response = super().to_representation(instance)
+        response['user'] = UserSerializers(instance.user).data
+        return response
 
 
 class CategorySerializer(serializers.ModelSerializer):
